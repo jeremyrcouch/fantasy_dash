98b18079132a58820f78ac188d41c0f886ec46f5
Jeremy Couch <jeremy.r.couch@gmail.com>
2020-04-14 11:48:56 -0400

    generic version of remain opp avg rank

--- app/app.py
+++ app/app.py
@@ -10,6 +10,33 @@ import dash_table
 from dash.dependencies import Input, Output
 
 
+GOOGLE_SHEETS_URL = "https://docs.google.com/spreadsheets/d/e/{}&single=true&output=csv"
+SCHEDULE_URL = "2PACX-1vS4BxaruR77zq40juWJSOIyTnXeM55dSFpUo1FKAS9MH2N5dX4B93eaTUafyiBVeg/pub?gid=1184397163"
+POINTS_URL = "2PACX-1vS4BxaruR77zq40juWJSOIyTnXeM55dSFpUo1FKAS9MH2N5dX4B93eaTUafyiBVeg/pub?gid=2069019650"
+
+WEEK_COL = "Week"
+PLAYER_COL = "Player"
+AGAINST_COL = "Against"
+POINTS_COL = "Points"
+RANK_COL = "Rank"
+COL_JOIN = "{} {}"
+
+CLOSE_MATCH_DIFF = 10
+
+COLORS = [
+    "#EC7063",
+    "#AF7AC5",
+    "#5DADE2",
+    "#48C9B0",
+    "#F9E79F",
+    "#E59866",
+    "#F06292",
+    "#58D68D",
+    "#AED6F1",
+    "#F8BBD0",
+]
+
+
 def determine_points_against(
     points_data: pd.DataFrame,
     schedule: pd.DataFrame,
@@ -49,7 +76,8 @@ def determine_points_against(
 
 
 def rank_points_to_avg_rank(sum_points: float, current_week: int) -> float:
-    """Average rank for player based on total rank points at current week.
+    """Average rank (1 highest, 10 lowest) based on total rank points at current week.
+    Assumes 10 players, 1st = 1, 10th = 0.1.
 
     Args:
         sum_points: float, sum of rank points through current week
@@ -102,9 +130,12 @@ def remaining_opponent_avg_rank(
                         season[player_col] == opp, COL_JOIN.format(rank_col, points_col)
                     ].iloc[0]
                 )
-            avg_ranks = [
-                rank_points_to_avg_rank(rp, current_week) for rp in rank_points
-            ]
+            # only works for 10 players, 0.1 to 1 rank pts per week
+            # if use this version, change column to "Remain Opp Avg Rank"
+            # avg_ranks = [
+            #     rank_points_to_avg_rank(rp, current_week) for rp in rank_points
+            # ]
+            avg_ranks = [rp/current_week for rp in rank_points]
             roar.append(round(sum(avg_ranks) / len(avg_ranks), 2))
         else:
             roar.append(0)
@@ -181,7 +212,7 @@ def collect_season_stats(
     season = season.reset_index().loc[:, season_col_order]
 
     current_week = points.loc[:, id_col].max()
-    season["Remain Opp Avg Rank"] = remaining_opponent_avg_rank(
+    season["Remain Opp Avg Rank Pts"] = remaining_opponent_avg_rank(
         season,
         schedule,
         current_week,
@@ -267,240 +298,214 @@ def update_season_dist_plot(week: int, y_col: str):
     return fig
 
 
-GOOGLE_SHEETS_URL = "https://docs.google.com/spreadsheets/d/e/{}&single=true&output=csv"
-SCHEDULE_URL = "2PACX-1vS4BxaruR77zq40juWJSOIyTnXeM55dSFpUo1FKAS9MH2N5dX4B93eaTUafyiBVeg/pub?gid=1184397163"
-POINTS_URL = "2PACX-1vS4BxaruR77zq40juWJSOIyTnXeM55dSFpUo1FKAS9MH2N5dX4B93eaTUafyiBVeg/pub?gid=2069019650"
-
-WEEK_COL = "Week"
-PLAYER_COL = "Player"
-AGAINST_COL = "Against"
-POINTS_COL = "Points"
-RANK_COL = "Rank"
-COL_JOIN = "{} {}"
-
-CLOSE_MATCH_DIFF = 10
-
-COLORS = [
-    "#EC7063",
-    "#AF7AC5",
-    "#5DADE2",
-    "#48C9B0",
-    "#F9E79F",
-    "#E59866",
-    "#F06292",
-    "#58D68D",
-    "#AED6F1",
-    "#F8BBD0",
-]
-
-app = Dash(
-    __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-)
-server = app.server
-
-# TODO: other sources
-schedule_wide = pd.read_csv(GOOGLE_SHEETS_URL.format(SCHEDULE_URL))
-points_wide = pd.read_csv(GOOGLE_SHEETS_URL.format(POINTS_URL)).dropna()
-# TODO: validation checks
-
-PLAYERS = [col for col in schedule_wide.columns if col != WEEK_COL]
-
-schedule = pd.melt(
-    schedule_wide, id_vars=[WEEK_COL], var_name=PLAYER_COL, value_name=AGAINST_COL
-)
-points_data = pd.melt(
-    points_wide, id_vars=[WEEK_COL], var_name=PLAYER_COL, value_name=POINTS_COL
-)
-points_against = determine_points_against(
-    points_data, schedule, WEEK_COL, PLAYER_COL, AGAINST_COL, POINTS_COL
-)
-points = pd.merge(points_data, points_against, how="left", on=[WEEK_COL, PLAYER_COL])
-
-current_week = points.loc[:, WEEK_COL].max()
-
-points["Won"] = (
-    points.loc[:, POINTS_COL] > points.loc[:, COL_JOIN.format(POINTS_COL, AGAINST_COL)]
-)
-# TODO: how to handle ties?
-
-points[RANK_COL] = points.groupby(WEEK_COL)[POINTS_COL].rank()
-points[COL_JOIN.format(AGAINST_COL, RANK_COL)] = points.groupby(WEEK_COL)[
-    COL_JOIN.format(POINTS_COL, AGAINST_COL)
-].rank()
-points[COL_JOIN.format(RANK_COL, POINTS_COL)] = points.loc[:, RANK_COL] / len(PLAYERS)
-points[COL_JOIN.format(COL_JOIN.format(RANK_COL, POINTS_COL), AGAINST_COL)] = points[
-    COL_JOIN.format(AGAINST_COL, RANK_COL)
-] / len(PLAYERS)
-
-points["Close Match"] = (
-    abs(
-        points.loc[:, POINTS_COL]
-        - points.loc[:, COL_JOIN.format(POINTS_COL, AGAINST_COL)]
-    )
-    <= CLOSE_MATCH_DIFF
-)
-points["Close Loss"] = (~points.loc[:, "Won"]) & points.loc[:, "Close Match"]
-points["Close Win"] = points.loc[:, "Won"] & points.loc[:, "Close Match"]
-
-# TODO: generic stat function??
-week_med = points.groupby(WEEK_COL)[POINTS_COL].median().reset_index()
-week_med = week_med.rename(columns={POINTS_COL: "Weekly Median Points"})
-points = pd.merge(points, week_med, on=WEEK_COL, how="left")
-points["Expected Win"] = (
-    points.loc[:, POINTS_COL] > points.loc[:, "Weekly Median Points"]
-)
-
-season = collect_season_stats(
-    points, WEEK_COL, PLAYER_COL, AGAINST_COL, POINTS_COL, RANK_COL
-)
-
-
-app.layout = html.Div(
-    [
-        html.Br(),
-        html.H5(WEEK_COL),
-        dcc.Slider(
-            id="week-slider",
-            min=schedule_wide[WEEK_COL].min(),
-            max=schedule_wide[WEEK_COL].max(),
-            value=current_week,
-            marks={
-                str(wk): (str(wk) if wk != current_week else "Current")
-                for wk in schedule_wide[WEEK_COL].unique()
-            },
-            step=None
-            # updatemode='drag'  - not fast enough to update as it's dragged
-        ),
-        html.Br(),
-        dcc.Graph(id="week-points"),
-        html.Br(),
-        dcc.Dropdown(
-            id="plot-selector",
-            options=[
-                {"label": lab, "value": col}
-                for lab, col in zip(
-                    [
-                        "Points",
-                        COL_JOIN.format(POINTS_COL, AGAINST_COL),
-                        COL_JOIN.format(RANK_COL, POINTS_COL),
-                        COL_JOIN.format(
-                            COL_JOIN.format(RANK_COL, POINTS_COL), AGAINST_COL
-                        ),
-                        "Opponents Season Score Rank (1 = opponent's lowest score of season, etc.)",
-                    ],
-                    [
-                        POINTS_COL,
-                        COL_JOIN.format(POINTS_COL, AGAINST_COL),
-                        COL_JOIN.format(RANK_COL, POINTS_COL),
-                        COL_JOIN.format(
-                            COL_JOIN.format(RANK_COL, POINTS_COL), AGAINST_COL
-                        ),
-                        COL_JOIN.format(AGAINST_COL, RANK_COL),
-                    ],
-                )
-            ],
-            value=POINTS_COL,
-            clearable=False,
-        ),
-        html.Br(),
-        dcc.Graph(id="season-dist-selected"),
-        html.Br(),
-        dash_table.DataTable(
-            id="season-stats-table",
-            columns=[{"name": col, "id": col} for col in season.columns],
-            style_table={"maxWidth": "900px"},
-            style_cell={"textAlign": "center"},
-            style_as_list_view=True,
-            style_header={"fontWeight": "bold"},
-            sort_action="native",
-            sort_mode="multi",
-        ),
-        html.Br(),
-    ],
-    style={"textAlign": "center"},
-)
-
-
-@app.callback(Output("week-points", "figure"), [Input("week-slider", "value")])
-def update_week_points_fig(week: int):
-    week_schedule = schedule.loc[schedule[WEEK_COL] == week, :]
-    if week > current_week:
-        week_points = [1] * len(PLAYERS)
-        week_players = PLAYERS.copy()
-        week_won = [0] * len(PLAYERS)
-        # return go.Figure([go.Bar(x=[], y=[])])
-    else:
-        week_points_df = points.loc[(points[WEEK_COL] == week), :]
-        week_points_df = week_points_df.sort_values(by=POINTS_COL, ascending=False)
-        week_points = week_points_df[POINTS_COL].to_list()
-        week_players = week_points_df[PLAYER_COL].to_list()
-        week_won = [int(won) for won in week_points_df["Won"].to_list()]
-    week_colors = [COLORS[PLAYERS.index(wp)] for wp in week_players]
-    matchup_numbers = [
-        "<b>{}</b>".format(i + 1) for i in range(int(len(week_players) / 2))
-    ]
-    week_texts = get_matchup_items(
-        week_players, week_schedule, matchup_numbers, PLAYER_COL, AGAINST_COL
-    )
-
-    week_points_fig = go.Figure(
-        [
-            go.Bar(
-                x=week_players,
-                y=week_points,
-                marker_color=week_colors,
-                marker_line_width=[2 * ww for ww in week_won],
-                marker_line_color="black",
-                text=week_texts,
-                textfont={"size": 16},
-                textposition="auto",
-                hoverinfo="y",
-            )
-        ]
-    )
-    week_points_fig.update_xaxes(tickfont={"size": 18})
-    week_points_fig.update_yaxes(
-        title_text=POINTS_COL, title_font={"size": 22}, tickfont={"size": 18}
-    )
-
-    week_points_fig.update_layout(
-        autosize=False,
-        height=400,
-        margin=go.layout.Margin(l=50, r=50, b=25, t=25, pad=4),
-        title=dict(
-            text="Numbers = matchup. Box outline = won.",
-            xref="paper",
-            yref="paper",
-            x=0.99,
-            y=0.8,
-            xanchor="right",
-            yanchor="bottom",
-        ),
-    )
-
-    return week_points_fig
-
-
-@app.callback(
-    Output("season-dist-selected", "figure"),
-    [Input("week-slider", "value"), Input("plot-selector", "value")],
-)
-def update_season_dist_pts_fig(week: int, col: str):
-    return update_season_dist_plot(week, col)
-
-
-@app.callback(Output("season-stats-table", "data"), [Input("week-slider", "value")])
-def update_season_stats_table(week: int):
-    temp_points = points.loc[points[WEEK_COL] <= week, :]
-    temp_points.loc[:, COL_JOIN.format(AGAINST_COL, RANK_COL)] = temp_points.groupby(
-        AGAINST_COL
-    )[COL_JOIN.format(POINTS_COL, AGAINST_COL)].rank()
-    temp_season = collect_season_stats(
-        temp_points, WEEK_COL, PLAYER_COL, AGAINST_COL, POINTS_COL, RANK_COL
-    )
-    return temp_season.to_dict("records")
-
-
 if __name__ == "__main__":
+
+    app = Dash(
+        __name__, external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+    )
+    server = app.server
+
+    # TODO: other sources
+    schedule_wide = pd.read_csv(GOOGLE_SHEETS_URL.format(SCHEDULE_URL))
+    points_wide = pd.read_csv(GOOGLE_SHEETS_URL.format(POINTS_URL)).dropna()
+    # TODO: validation checks
+
+    PLAYERS = [col for col in schedule_wide.columns if col != WEEK_COL]
+
+    schedule = pd.melt(
+        schedule_wide, id_vars=[WEEK_COL], var_name=PLAYER_COL, value_name=AGAINST_COL
+    )
+    points_data = pd.melt(
+        points_wide, id_vars=[WEEK_COL], var_name=PLAYER_COL, value_name=POINTS_COL
+    )
+    points_against = determine_points_against(
+        points_data, schedule, WEEK_COL, PLAYER_COL, AGAINST_COL, POINTS_COL
+    )
+    points = pd.merge(points_data, points_against, how="left", on=[WEEK_COL, PLAYER_COL])
+
+    current_week = points.loc[:, WEEK_COL].max()
+
+    points["Won"] = (
+        points.loc[:, POINTS_COL] > points.loc[:, COL_JOIN.format(POINTS_COL, AGAINST_COL)]
+    )
+    # TODO: how to handle ties?
+
+    points[RANK_COL] = points.groupby(WEEK_COL)[POINTS_COL].rank()
+    points[COL_JOIN.format(AGAINST_COL, RANK_COL)] = points.groupby(WEEK_COL)[
+        COL_JOIN.format(POINTS_COL, AGAINST_COL)
+    ].rank()
+    points[COL_JOIN.format(RANK_COL, POINTS_COL)] = points.loc[:, RANK_COL] / len(PLAYERS)
+    points[COL_JOIN.format(COL_JOIN.format(RANK_COL, POINTS_COL), AGAINST_COL)] = points[
+        COL_JOIN.format(AGAINST_COL, RANK_COL)
+    ] / len(PLAYERS)
+
+    points["Close Match"] = (
+        abs(
+            points.loc[:, POINTS_COL]
+            - points.loc[:, COL_JOIN.format(POINTS_COL, AGAINST_COL)]
+        )
+        <= CLOSE_MATCH_DIFF
+    )
+    points["Close Loss"] = (~points.loc[:, "Won"]) & points.loc[:, "Close Match"]
+    points["Close Win"] = points.loc[:, "Won"] & points.loc[:, "Close Match"]
+
+    # TODO: generic stat function??
+    week_med = points.groupby(WEEK_COL)[POINTS_COL].median().reset_index()
+    week_med = week_med.rename(columns={POINTS_COL: "Weekly Median Points"})
+    points = pd.merge(points, week_med, on=WEEK_COL, how="left")
+    points["Expected Win"] = (
+        points.loc[:, POINTS_COL] > points.loc[:, "Weekly Median Points"]
+    )
+
+    season = collect_season_stats(
+        points, WEEK_COL, PLAYER_COL, AGAINST_COL, POINTS_COL, RANK_COL
+    )
+
+
+    app.layout = html.Div(
+        [
+            html.Br(),
+            html.H5(WEEK_COL),
+            dcc.Slider(
+                id="week-slider",
+                min=schedule_wide[WEEK_COL].min(),
+                max=schedule_wide[WEEK_COL].max(),
+                value=current_week,
+                marks={
+                    str(wk): (str(wk) if wk != current_week else "Current")
+                    for wk in schedule_wide[WEEK_COL].unique()
+                },
+                step=None
+                # updatemode='drag'  - not fast enough to update as it's dragged
+            ),
+            html.Br(),
+            dcc.Graph(id="week-points"),
+            html.Br(),
+            dcc.Dropdown(
+                id="plot-selector",
+                options=[
+                    {"label": lab, "value": col}
+                    for lab, col in zip(
+                        [
+                            "Points",
+                            COL_JOIN.format(POINTS_COL, AGAINST_COL),
+                            COL_JOIN.format(RANK_COL, POINTS_COL),
+                            COL_JOIN.format(
+                                COL_JOIN.format(RANK_COL, POINTS_COL), AGAINST_COL
+                            ),
+                            "Opponents Season Score Rank (1 = opponent's lowest score of season, etc.)",
+                        ],
+                        [
+                            POINTS_COL,
+                            COL_JOIN.format(POINTS_COL, AGAINST_COL),
+                            COL_JOIN.format(RANK_COL, POINTS_COL),
+                            COL_JOIN.format(
+                                COL_JOIN.format(RANK_COL, POINTS_COL), AGAINST_COL
+                            ),
+                            COL_JOIN.format(AGAINST_COL, RANK_COL),
+                        ],
+                    )
+                ],
+                value=POINTS_COL,
+                clearable=False,
+            ),
+            html.Br(),
+            dcc.Graph(id="season-dist-selected"),
+            html.Br(),
+            dash_table.DataTable(
+                id="season-stats-table",
+                columns=[{"name": col, "id": col} for col in season.columns],
+                style_table={"maxWidth": "900px"},
+                style_cell={"textAlign": "center"},
+                style_as_list_view=True,
+                style_header={"fontWeight": "bold"},
+                sort_action="native",
+                sort_mode="multi",
+            ),
+            html.Br(),
+        ],
+        style={"textAlign": "center"},
+    )
+
+
+    @app.callback(Output("week-points", "figure"), [Input("week-slider", "value")])
+    def update_week_points_fig(week: int):
+        week_schedule = schedule.loc[schedule[WEEK_COL] == week, :]
+        if week > current_week:
+            week_points = [1] * len(PLAYERS)
+            week_players = PLAYERS.copy()
+            week_won = [0] * len(PLAYERS)
+            # return go.Figure([go.Bar(x=[], y=[])])
+        else:
+            week_points_df = points.loc[(points[WEEK_COL] == week), :]
+            week_points_df = week_points_df.sort_values(by=POINTS_COL, ascending=False)
+            week_points = week_points_df[POINTS_COL].to_list()
+            week_players = week_points_df[PLAYER_COL].to_list()
+            week_won = [int(won) for won in week_points_df["Won"].to_list()]
+        week_colors = [COLORS[PLAYERS.index(wp)] for wp in week_players]
+        matchup_numbers = [
+            "<b>{}</b>".format(i + 1) for i in range(int(len(week_players) / 2))
+        ]
+        week_texts = get_matchup_items(
+            week_players, week_schedule, matchup_numbers, PLAYER_COL, AGAINST_COL
+        )
+
+        week_points_fig = go.Figure(
+            [
+                go.Bar(
+                    x=week_players,
+                    y=week_points,
+                    marker_color=week_colors,
+                    marker_line_width=[2 * ww for ww in week_won],
+                    marker_line_color="black",
+                    text=week_texts,
+                    textfont={"size": 16},
+                    textposition="auto",
+                    hoverinfo="y",
+                )
+            ]
+        )
+        week_points_fig.update_xaxes(tickfont={"size": 18})
+        week_points_fig.update_yaxes(
+            title_text=POINTS_COL, title_font={"size": 22}, tickfont={"size": 18}
+        )
+
+        week_points_fig.update_layout(
+            autosize=False,
+            height=400,
+            margin=go.layout.Margin(l=50, r=50, b=25, t=25, pad=4),
+            title=dict(
+                text="Numbers = matchup. Box outline = won.",
+                xref="paper",
+                yref="paper",
+                x=0.99,
+                y=0.8,
+                xanchor="right",
+                yanchor="bottom",
+            ),
+        )
+
+        return week_points_fig
+
+
+    @app.callback(
+        Output("season-dist-selected", "figure"),
+        [Input("week-slider", "value"), Input("plot-selector", "value")],
+    )
+    def update_season_dist_pts_fig(week: int, col: str):
+        return update_season_dist_plot(week, col)
+
+
+    @app.callback(Output("season-stats-table", "data"), [Input("week-slider", "value")])
+    def update_season_stats_table(week: int):
+        temp_points = points.loc[points[WEEK_COL] <= week, :]
+        temp_points.loc[:, COL_JOIN.format(AGAINST_COL, RANK_COL)] = temp_points.groupby(
+            AGAINST_COL
+        )[COL_JOIN.format(POINTS_COL, AGAINST_COL)].rank()
+        temp_season = collect_season_stats(
+            temp_points, WEEK_COL, PLAYER_COL, AGAINST_COL, POINTS_COL, RANK_COL
+        )
+        return temp_season.to_dict("records")
+
     app.run_server(debug=True)
 
