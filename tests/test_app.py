01b25313a4bcad1d45dfb1db22865dc47f3f9d48
Jeremy Couch <jeremy.r.couch@gmail.com>
2020-04-15 21:35:25 -0400

    added remaining tests and test data

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import pytest
 
-from app.app import rank_points_to_avg_rank, determine_points_against
+from app.app import (rank_points_to_avg_rank, determine_points_against,
+    remaining_opponent_avg_rank, collect_season_stats, get_matchup_items)
 from app.app import WEEK_COL, PLAYER_COL, AGAINST_COL, POINTS_COL, RANK_COL, COL_JOIN
 
 
@@ -16,6 +17,16 @@ def schedule_wide():
 @pytest.fixture
 def points_wide():
     return pd.read_csv('{}{}'.format(DATA_DIR, 'points.csv'))
+
+
+@pytest.fixture
+def points_complete():
+    return pd.read_csv('{}{}'.format(DATA_DIR, 'points_complete.csv'))
+
+
+@pytest.fixture
+def season():
+    return pd.read_csv('{}{}'.format(DATA_DIR, 'season.csv'))
 
 
 def test_determine_points_against(points_wide, schedule_wide) -> float:
@@ -53,3 +64,58 @@ def test_rank_points_to_avg_rank(rank_points, current_week, expected_avg) -> flo
     assert isinstance(avg_rank, float)
     assert avg_rank == expected_avg
 
+
+@pytest.mark.parametrize('current_week',
+    [pytest.param(10, id='middle'),
+     pytest.param(14, id='last')])
+def test_remaining_opponent_avg_rank(current_week, season, schedule_wide):
+    # arrange
+    schedule = pd.melt(
+        schedule_wide, id_vars=[WEEK_COL], var_name=PLAYER_COL, value_name=AGAINST_COL
+    )
+
+    # act
+    roar = remaining_opponent_avg_rank(season, schedule, current_week,
+        WEEK_COL, PLAYER_COL, AGAINST_COL, POINTS_COL, RANK_COL)
+
+    # assert
+    assert isinstance(roar, list)
+    if current_week == schedule[WEEK_COL].max():
+        assert all([r == 0 for r in roar])
+    else:
+        assert min(roar) > 0
+
+
+def test_collect_season_stats(season, points_complete, schedule_wide):
+    # arrange
+    schedule = pd.melt(
+        schedule_wide, id_vars=[WEEK_COL], var_name=PLAYER_COL, value_name=AGAINST_COL
+    )
+
+    # act
+    season_test = collect_season_stats(points_complete, schedule, WEEK_COL,
+        PLAYER_COL, AGAINST_COL, POINTS_COL, RANK_COL)
+
+    # assert
+    assert list(season_test.columns) == list(season.columns)
+    assert season_test['Place'].to_list() == sorted(season_test['Place'].to_list())
+
+
+def test_get_matchup_items(points_complete, schedule_wide):
+    # arrange
+    week = 10
+    schedule = pd.melt(
+        schedule_wide, id_vars=[WEEK_COL], var_name=PLAYER_COL, value_name=AGAINST_COL
+    )
+    week_schedule = schedule.loc[schedule[WEEK_COL] == week, :]
+    week_players = [col for col in schedule_wide.columns if col != WEEK_COL]
+    items = ['{}'.format(i + 1) for i in range(int(len(week_players) / 2))]
+    expected_items = ['1', '1', '2', '2', '3', '4', '3', '5', '5', '4']
+
+    # act
+    matchup_items = get_matchup_items(week_players, week_schedule, items,
+        PLAYER_COL, AGAINST_COL)
+
+    # assert
+    assert isinstance(matchup_items, list)
+    assert matchup_items == expected_items
